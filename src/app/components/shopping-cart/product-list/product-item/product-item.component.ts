19aca9504cbcee0f1256487570b1e0c234770407
siena giacoma <sgiacoma2023@chadwickschool.org>
2021-09-13 20:45:12 -0700

    feat: updated product list

--- src/app/components/shopping-cart/product-list/product-item/product-item.component.ts
+++ src/app/components/shopping-cart/product-list/product-item/product-item.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/product'
+ 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
+
 export class ProductItemComponent implements OnInit {
 
+  @Input() productItem: Product
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    
   }
 
 }
